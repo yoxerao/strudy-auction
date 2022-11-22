86ab24b876a201e91230dadd3a291c69658842c7
Carolina Brandão <up202004461@edu.fc.up.pt>
2022-11-22 15:14:31 +0000

    link ref to user profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,8 @@
       <header>
         <h1><a href="{{ url('/cards') }}">eAuction</a></h1>
         @if (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
+        <a class="button" href="{{ url('/logout') }}"> Logout </a>
+        <a href="/user/{{ Auth::user()->id }}"> {{ Auth::user()->name }}</a>
         @else
         <a class="button" href="{{ url('/login') }}"> Login </a>
         <a class="button" href="{{ url('/register') }}"> Register </a>
