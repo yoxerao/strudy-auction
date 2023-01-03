0dad1b3fe18884726dec63deda795a07da27d0f3
up202006345 <up202006345@fe.up.pt>
2023-01-03 19:46:30 +0000

    changed way follows are added to db

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,8 +37,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-    
-    public function getUser(){
+
+    public function getUser(Request $request)
+    {
         return $request->user();
     }
 
@@ -45,5 +47,4 @@ class LoginController extends Controller
     {
         return 'username';
     }
-
 }
--- app/Http/Controllers/UserFollowAuctionController.php
+++ app/Http/Controllers/UserFollowAuctionController.php
@@ -10,25 +10,25 @@ use App\Models\UserFollowAuction;
 
 class UserFollowAuctionController extends Controller
 {
-    public function follow(int $idAuction)
-    {
-        $alreadyFollow = UserFollowAuction::where('id_user', Auth::id())
-                                            ->where('id_auction', $idAuction)
-                                            ->first();
-        
-        if($alreadyFollow == null) {
-         
-          $idUser = Auth::id();
-          $data=array('id_user'=>$idUser, 'id_auction'=>$idAuction);
-          DB::table('user_follow_auction')->insert($data);
-        }
+  public function follow(int $idAuction)
+  {
+    $alreadyFollow = UserFollowAuction::where('id_user', Auth::id())->where('id_auction', $idAuction)->first();
 
-        return redirect("/auctions");
+    if ($alreadyFollow == null) {
+
+      $idUser = Auth::id();
+      $userFollowAuction = new UserFollowAuction;
+      $userFollowAuction->id_user = $idUser;
+      $userFollowAuction->id_auction = $idAuction;
+      $userFollowAuction->save();
     }
 
-    public function list(int $idAuction)
-    {
-      $followers = UserFollowAuction::all()->where('id_auction', $idAuction);
-      return view('pages.followers', ['followers' => $followers]);
-    }
+    return redirect("/auctions");
+  }
+
+  public function list(int $idAuction)
+  {
+    $followers = UserFollowAuction::all()->where('id_auction', $idAuction);
+    return view('pages.followers', ['followers' => $followers]);
+  }
 }
