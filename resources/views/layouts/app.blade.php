b746776987c84c62b28628c6efe55d8b1c567d8d
up202003474 <up202003474@fe.up.pt>
2023-01-03 18:11:28 +0000

    added boot strap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script type="text/javascript">
-        // Fix for Firefox autofocus CSS bug
-        // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
-    </script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
-    <script type="text/javascript" src={{ asset('js/timer.js') }} defer></script>
-  </head>
-  <body>
-    <main>
-      <header>
-        <h1><a href="{{ url('/') }}">eAuction</a></h1>
-        @if (Auth::guard('admin')->check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a>
-        <a href="/user/{{ Auth::guard('admin')->user()->id }}"> {{ Auth::guard('admin')->user()->name }}</a>
-        @elseif (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a>
-        <a href="/user/{{ Auth::user()->id }}"> {{ Auth::user()->name }}</a>
-        @else
-        <a class="button" href="{{ url('/login') }}"> Login </a>
-        <a class="button" href="{{ url('/register') }}"> Register </a>
-        @endif
-      </header>
-      <section id="content">
-        @yield('content')
-      </section>
-    </main>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <footer style="text-align:center;">
-      <a class="button" href="{{ url('/about') }}"> About us </a>
-      <a class="button" href="{{ url('/faq') }}"> FAQ's </a>
-    </footer>
-  </body>
+  <!-- Styles -->
+  <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script type="text/javascript">
+    // Fix for Firefox autofocus CSS bug
+    // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
+  </script>
+  <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
+  <script type="text/javascript" src={{ asset('js/timer.js') }} defer></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="<?php echo e(asset('css/homepage.css')); ?>" rel="stylesheet">
+</head>
+
+<body>
+  <main>
+    <header>
+      <h1><a href="{{ url('/') }}">eAuction</a></h1>
+      @if (Auth::guard('admin')->check())
+      <a class="button" href="{{ url('/logout') }}"> Logout </a>
+      <a href="/user/{{ Auth::guard('admin')->user()->id }}"> {{ Auth::guard('admin')->user()->name }}</a>
+      @elseif (Auth::check())
+      <a class="button" href="{{ url('/logout') }}"> Logout </a>
+      <a href="/user/{{ Auth::user()->id }}"> {{ Auth::user()->name }}</a>
+      @else
+      <a class="button" href="{{ url('/login') }}"> Login </a>
+      <a class="button" href="{{ url('/register') }}"> Register </a>
+      @endif
+    </header>
+    <section id="content">
+      @yield('content')
+    </section>
+  </main>
+
+  <footer style="text-align:center;">
+    <a class="button" href="{{ url('/about') }}"> About us </a>
+    <a class="button" href="{{ url('/faq') }}"> FAQ's </a>
+  </footer>
+</body>
+
 </html>
